fb77c78342a5bf99b670f9a4573d36a571ba5049
Teoteo11 <49559030+Teoteo11@users.noreply.github.com>
2019-05-03 09:20:42 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Grid board = new Grid();
-        String gameTurn;
-        gameTurn=board.played(5,1);
-        System.out.println(gameTurn);
-        gameTurn=board.played(4,5);
-        System.out.println(gameTurn);
-        gameTurn=board.played(2,1);
-        System.out.println(gameTurn);
-    }
+        
+        
 }
